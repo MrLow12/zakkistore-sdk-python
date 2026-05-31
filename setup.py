253c53dd145ec199909a85c8a78ce840632a2729
zakkiXD-Dev <75411357+MrLow12@users.noreply.github.com>
2026-06-01 04:09:44 +0700

    Update setup.py with complete initialization parameters

---
+++ setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="zakkistore-sdk",
+    version="1.0.1",
+    description="Official Python SDK Client Library for Zakki Store B2B API",
+    long_description=open("README.md", encoding="utf-8").read(),
+    long_description_content_type="text/markdown",
+    author="ZakkiXD",
+    author_email="b2b_partner@example.com",
+    url="https://github.com/MrLow12/zakkistore-sdk",
+    packages=find_packages(),
+    install_requires=[
+        "requests>=2.25.0"
+    ],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.6",
+)
